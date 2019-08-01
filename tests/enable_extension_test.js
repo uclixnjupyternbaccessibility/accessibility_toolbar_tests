93226a34d3ccf4dac199b117d91eb39c945c3914
Joshua Zeltser <joshua.zeltser@gmail.com>
2019-08-01 16:12:44 +0100

    added more testing stuff

--- tests/enable_extension_test.js
+++ tests/enable_extension_test.js
@@ -20,28 +20,28 @@ let driver = global.driver;
     describe('EnableExtensionTest', () => {
        it('verify accessibility toolbar extension exists', async () => {
            await driver.get('http://host.docker.internal:10000/');
-
+           await driver.sleep(5000);
            let psw = await driver.findElement(By.name('password'));
 
            await psw.sendKeys('12345678');
 
            await psw.sendKeys(webdriver.Key.ENTER);
 
-           await driver.sleep(2000);
+           await driver.sleep(5000);
 
            await driver.findElement(By.linkText("Nbextensions")).click();
 
-           await driver.sleep(1000);
+           await driver.sleep(5000);
 
            let text = await driver.findElement(webdriver.By.linkText('Accessibility Toolbar')).getText();
            expect(text).to.equal("Accessibility Toolbar");
 
            await driver.findElement(By.xpath("//*[@id=\"tabs\"]/li[4]/a")).click();
-           await driver.sleep(1000);
+           await driver.sleep(5000);
            await driver.findElement(By.xpath("//*[@id=\"nbextensions-configurator-container\"]/div[3]/div[1]/div[5]/button[1]")).click();
-           await driver.sleep(1000);
+           await driver.sleep(5000);
            await driver.findElement(By.linkText("Files")).click();
-           await driver.sleep(1000);
+           await driver.sleep(5000);
 
        });
        after(async () => driver.quit());
