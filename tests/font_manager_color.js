c55a7fa0747606dca38d6633d5a4edad583959d3
Norah Abanumay <norah@eduroam-int-dhcp-97-34-155.ucl.ac.uk>
2019-08-01 15:00:54 +0100

    create font_manager_color file

---
+++ tests/font_manager_color.js
@@ -0,0 +1,53 @@
+const { expect } = require('chai');
+
+let webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+let driver = global.driver;
+
+(async function run_tests(driver) {
+
+    describe('EnableExtensionTest', () => {
+       it('verify accessibility toolbar extension exists', async () => {
+           await driver.get('http://host.docker.internal:10000/');
+
+           let psw = await driver.findElement(By.name('password'));
+
+           await psw.sendKeys('12345678');
+
+           await psw.sendKeys(webdriver.Key.ENTER);
+
+           await driver.sleep(2000);
+
+           await driver.findElement(By.linkText("Nbextensions")).click();
+
+           await driver.sleep(1000);
+       });
+
+       it('Create new notebook', async () => {
+           await driver.get('http://host.docker.internal:10000/');
+           await driver.sleep(2000);
+           await driver.findElement(By.linkText("work")).click();
+           await driver.findElement(By.id("new-dropdown-button")).click();
+           await driver.findElement(By.linkText("Python 3")).click();
+
+       });
+//==========================================
+it('Create new notebook', async () => {
+    await driver.get('http://host.docker.internal:10000/');
+    await driver.sleep(2000);
+    await driver.findElement(By.linkText("work")).click();
+    await driver.findElement(By.id("new-dropdown-button")).click();
+    await driver.findElement(By.linkText("Python 3")).click();
+
+});
+
+
+
+//=============================================
+       after(async () => driver.quit());
+    });
+
+
+})(driver);
