340c7d9b352d1df47033ba5ca9ad7f9d70529431
Joshua Zeltser <joshua.zeltser@gmail.com>
2019-08-02 18:27:30 +0100

    started to add delete style tests

--- tests/predefined_fonts_test.js
+++ tests/predefined_fonts_test.js
@@ -30,7 +30,6 @@ let driver = global.driver ? global.driver : new webdriver.Builder().forBrowser(
             });
         });
 
-
         it('create new predefined style test', async () => {
             await driver.wait(until.elementLocated(By.id("predefined_styles"))).click();
             var new_style_button = await driver.wait(until.elementLocated(By.id("new_style_button")));
@@ -38,17 +37,48 @@ let driver = global.driver ? global.driver : new webdriver.Builder().forBrowser(
             await new_style_button.findElement(By.xpath("./..")).click();
 
             var new_style = driver.wait(until.elementLocated(By.id("style_name")));
-            await new_style.sendKeys('predefined_style_1');
+            await driver.sleep(1000);
+                await new_style.sendKeys('predefined_style_1');
 
             await driver.wait(until.elementLocated(By.id('save-button'))).click();
 
-
-            driver.wait(webdriver.until.alertIsPresent()).then(await driver.switchTo().alert().accept()).catch();
-            await driver.sleep(3000)
+            driver.switchTo().alert().then(
+                function (alert) {alert.accept();},
+                function (error) {}
+            );
+            // await driver.sleep(3000)
             await driver.wait(until.elementLocated(By.id("fs"))).click();
             await driver.wait(until.elementLocated(By.id("predefined_styles"))).click();
-            // await driver.close();
+            let styles = driver.wait(until.elementLocated(By.linkText('predefined_style_1')));
+            await styles.getAttribute('class').then(function(classes) {
+                expect(classes).to.include('dropdown-item-checked');
+            });
         });
+
+        it('delete an existing predefined style test', async () => {
+            await driver.wait(until.elementLocated(By.id("predefined_styles"))).click();
+            var delete_style_button = await driver.wait(until.elementLocated(By.id("delete_style_button")));
+            await driver.sleep(1000);
+            await delete_style_button.findElement(By.xpath("./..")).click();
+            await driver.sleep(1000);
+            driver.wait(until.elementLocated(By.linkText('predefined_style_1'))).click();
+
+            var styles = driver.findElement(By.xpath('//*[@id="style-list"]'));
+            for (var i; i < styles.length; i++) {
+                console.log(style);
+                style[i] === 'predefined_style_1' ? style[i].click() : '';
+            }
+            await driver.wait(until.elementLocated(By.id("delete_button"))).click();
+            driver.switchTo().alert().then(
+                function (alert) {alert.accept();},
+                function (error) {}
+            );
+
+            let text = await driver.findElement(webdriver.By.linkText('predefined_style_1')).getText();
+            expect(text.length).to.equal(0);
+        });
+
+
     });
     after(async () => driver.quit());
 
